99b17ca5e38f96c3b423dbd0fb118410c4191601
rohitsainissn <rohitsainissngmail.com>
2023-11-26 13:10:44 +0530

    just the project is working in local by rohitsainidtu mongoDB

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "rohit_projects"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://rohitsainidtu:jairamjai@cluster0.rmtgsmy.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
