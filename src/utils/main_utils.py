48fa62f5bd9d889ecaa62ad9467f26eabef5360b
rohitsainissn <rohitsainissngmail.com>
2023-11-27 21:20:08 +0530

    second

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -64,12 +64,12 @@ class MainUtils:
         except Exception as e:
             raise CustomException(e, sys) from e
    
-    @staticmethod     
-    def load_object(file_path):
-        try:
-            with open(file_path,'rb') as file_obj:
-                return pickle.load(file_obj)
-        except Exception as e:
-            logging.info('Exception Occured in load_object function utils')
-            raise CustomException(e,sys)
+    # @staticmethod     
+    # def load_object(file_path):
+    #     try:
+    #         with open(file_path,'rb') as file_obj:
+    #             return pickle.load(file_obj)
+    #     except Exception as e:
+    #         logging.info('Exception Occured in load_object function utils')
+    #         raise CustomException(e,sys)
     
